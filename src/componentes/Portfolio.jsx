0b0dce766503c42afc4e6a5910f8104fddca46dd
Victor C <victorcamacaro253@gmail.com>
2024-12-01 23:32:10 -0400

    Add files via upload

--- src/componentes/Portfolio.jsx
+++ src/componentes/Portfolio.jsx
@@ -1,4 +1,5 @@
-
+import { useEffect,useContext } from 'react';
+import { LanguageContext } from '../contexts/languageContext';
 import proyecto1 from '../../assets/images/datacenter.webp';
 import proyecto2 from '../../assets/images/php.png';
 import proyecto3 from '../../assets/images/basededatos.jpg';
@@ -7,6 +8,10 @@ import proyecto5 from '../../assets/images/responsive-web.webp';
 import proyecto6 from '../../assets/images/api_pic.jpg';
 
 function Portfolio() {
+  const { language,texts } = useContext(LanguageContext);
+  const { network,networkP,backDev,backDevP,database,databaseP,analysis,analysisP,webDev,webDevP,API,APIP
+     } = texts.Portfolio[0][language] || texts.Portfolio[0]['es']; // Fallback a español si no se encuentra el idioma
+
   return (
     <section className="portfolio" id="portfolio">
       <div className="contenido-seccion">
@@ -21,8 +26,8 @@ function Portfolio() {
 
             
               <div className="overlay">
-						<h3>Redes</h3>
-						<p>Interconexion de redes</p>
+						<h3>{network}</h3>
+						<p>{networkP}</p>
 						
 					</div>
             
@@ -35,8 +40,8 @@ function Portfolio() {
             <div className="hover-galeria">
               <img src={proyecto2} alt="hover" style={{ height: '255px' }}  />
               <div className="overlay">
-                        <h3>Desarrollo Backend</h3>
-                        <p>PHP</p>
+                        <h3>{backDev}</h3>
+                        <p>{backDevP}</p>
                     </div>
             </div>
           </div>
@@ -48,8 +53,8 @@ function Portfolio() {
             <div className="hover-galeria">
               <img src={proyecto3} alt="hover"  style={{ height: '255px', width:'340px' }} />
               <div className="overlay">
-                        <h3>Base de Datos</h3>
-                        <p>SQL</p>
+                        <h3>{database}</h3>
+                        <p>{databaseP}</p>
                     </div>
             </div>
           </div>
@@ -61,8 +66,8 @@ function Portfolio() {
             <div className="hover-galeria">
               <img src={proyecto4} alt="hover"  style={{ height: '255px' }} />
               <div className="overlay">
-                        <h3>Analisis de sistema</h3>
-                        <p>Desarrollo de sistema</p>
+                        <h3>{analysis}</h3>
+                        <p>{analysisP}</p>
                     </div>
             </div>
           </div>
@@ -75,8 +80,8 @@ function Portfolio() {
             <div className="hover-galeria">
               <img src={proyecto5} alt="hover"  style={{ height: '255px' }} />
               <div className="overlay">
-                        <h3>Desarrollo Web</h3>
-                        <p>PHP,HTML,CSS,JAVASCRIPT</p>
+                        <h3>{webDev}</h3>
+                        <p>{webDevP}</p>
                     </div>
             </div>
           </div>
@@ -89,8 +94,8 @@ function Portfolio() {
             <div className="hover-galeria">
               <img src={proyecto6} alt="hover"  style={{ height: '255px' }} />
               <div className="overlay">
-                        <h3>API</h3>
-                        <p>Desarrollo de API</p>
+                        <h3>{API}</h3>
+                        <p>{APIP}</p>
                     </div>
             </div>
           </div>
